55d8ebd560ea6ab83a92767986adbc86fb47bd50
AliV404 <alibv-bb209@code.edu.az>
2026-03-28 20:24:56 +0300

    adding allowed url

--- SmartPortfolio.API/Program.cs
+++ SmartPortfolio.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendDevPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") 
+        policy.WithOrigins("http://localhost:5173", "https://smart-portfolio-front.vercel.app") 
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
